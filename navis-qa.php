addd86a7aa388e923a946b8cde91dd2cd53402f0
Chris Amico <eyeseast@gmail.com>
2011-08-23 11:58:35 -0400

    Skeleton of the plugin class

--- navis-qa.php
+++ navis-qa.php
@@ -23,4 +23,69 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+class Navis_QA {
+        
+    function __construct() {
+        // add tinymce buttons
+        // add admin styles
+        // include qa css
+        
+        // tinymce plugin
+        add_action('init', array(&$this, 'register_tinymce_filters'));
+        
+        // scripts & styles
+        add_action( 'admin_print_scripts-post.php', 
+            array( &$this, 'register_admin_scripts' )
+        );
+        add_action( 'admin_print_scripts-post-new.php', 
+            array( &$this, 'register_admin_scripts' )
+        );
+        add_action( 'wp_print_scripts',
+            array( &$this, 'register_scripts' )
+        );
+        add_action( 
+            'admin_print_styles-post.php', array( &$this, 'add_stylesheet' ) 
+        );
+        add_action( 
+            'admin_print_styles-post-new.php', 
+            array( &$this, 'add_stylesheet' ) 
+        );
+        
+    }
+    
+    function register_tinymce_filters() {
+        add_filter('mce_external_plugins', 
+            array(&$this, 'add_tinymce_plugin')
+        );
+        add_filter('mce_buttons', 
+            array(&$this, 'register_button')
+        );
+    }
+    
+    function add_tinymce_plugin($plugins_array) {
+        
+        return $plugins_array;
+    }
+    
+    function register_button($buttons) {
+        array_push($buttons, '|', 'question', 'answer', '|');
+        return $buttons;
+    }
+    
+    
+    function register_admin_scripts() {
+        
+    }
+    
+    function register_scripts() {
+        
+    }
+    
+    function add_stylesheet() {
+        
+    }
+}
+
+new Navis_QA;
+
 ?>
